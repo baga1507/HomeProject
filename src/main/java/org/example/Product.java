5954bdd0039fedcdd8790855b2c37d60d46c4c3e
baga1507 <baga1507@yandex.ru>
2023-02-05 22:46:28 +0300

    OK

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String title;
+    private Integer price;
+
+    public Product (ProductBuilder builder) {
+        this.title = builder.title;
+        this.price = builder.price;
+    }
+
+    public static class ProductBuilder {
+        String title;
+        Integer price;
+
+        public ProductBuilder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public ProductBuilder setPrice(Integer price) {
+            this.price = price;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
+    }
+}
